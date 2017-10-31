da0efb4dde6475e61017c1645fa5c3364e3acb8b
高毓谦 <yuqian.gao@mljr.com>
2017-10-31 21:07:21 +0800

    first status

--- vuetest/src/router/index.js
+++ vuetest/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import first from '@/components/notefirst'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'first',
+      component: first
     }
   ]
 })
